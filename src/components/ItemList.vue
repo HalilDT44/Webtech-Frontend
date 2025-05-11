f00e029c81a875b5e96ce3b194f2473b4d2ac936
Halil <halil.tingir@student.htw-berlin.de>
2025-05-11 22:55:59 +0200

    Änderungen an ItemList.vue speichern

---
+++ src/components/ItemList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ItemList',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
